8f8fbd947458a42b6aa5e1daca1422cf893f62fe
Iain Taylor <iain.taylor.94@gmail.com>
2023-09-16 14:30:23 +0000

    completed program

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
+// Macro to find the minimum of three values
+
 #include <stdio.h>
 
+#define MIN2(x, y) (x) < (y) ? (x) : (y)
+#define MIN3(x, y, z) MIN2((x),MIN2((y), (z)))
+
 int main(void) {
-  printf("Hello World\n");
+
+  int x = 4, y = 9, z = 1;
+  printf ("The minimum of %d, %d and %d is %d\n", x, y, z, MIN3(x, y, z));
+  
   return 0;
 }
